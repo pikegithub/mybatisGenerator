e15dd5d3f2166cbd4be2433bfd36104223e83520
pike <pikegithub@protonmail.com>
2023-06-08 14:23:33 +0700

    commit

--- src/main/java/org/mybatis/RespEnum/RespEnum.java
+++ src/main/java/org/mybatis/RespEnum/RespEnum.java
@@ -1,34 +1,34 @@
 package org.mybatis.RespEnum;
 
 /**
- * @Author  carl
+ * @Author carl
  * @Description 通用响应编码枚举常量
  */
 public enum RespEnum {
-    RESP_500("500","请求失败"),
-    RESP_00("00","请求成功");
+    RESP_500("500", "请求失败"),
+    RESP_00("00", "请求成功");
 
     private String code;
     private String desc;
 
-    RespEnum(String code,String desc) {
+    RespEnum(String code, String desc) {
         this.code = code;
         this.desc = desc;
     }
 
-    public String getCode(){
+    public String getCode() {
         return code;
     }
 
-    public void setCode(String code){
+    public void setCode(String code) {
         this.code = code;
     }
 
-    public String getDesc(){
+    public String getDesc() {
         return desc;
     }
 
-    public void setDesc(String Desc){
+    public void setDesc(String Desc) {
         this.desc = desc;
     }
 
